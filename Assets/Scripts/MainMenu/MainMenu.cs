6067d0e3489a3aac9f8c67d6c30250ea71e26e8f
Palesa2323 <palesagaetsewe52@gmail.com>
2025-09-24 19:11:42 +0200

    Done w/UI

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Needed to load scenes
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("OfficeLevels"); // Make sure your game scene is named correctly
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting..."); // Works only in a built game
+    }
+}
